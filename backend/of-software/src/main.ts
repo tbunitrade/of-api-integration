05ba1c99f9ac57318a95922d01b005705dadea37
webeng101 <jmawn101@gmail.com>
2024-05-21 17:32:27 +0200

    no message

--- backend/of-software/src/main.ts
+++ backend/of-software/src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
   // The number of milliseconds of inactivity a server needs to wait for additional incoming data
   // server.keepAliveTimeout = 1 * 60 * 60 * 1000;
   // Limit the amount of time the parser will wait to receive the complete HTTP headers
-  // server.headersTimeout = 1 * 60 * 61 * 1000;
+  server.headersTimeout = 1 * 60 * 61 * 1000;
   // server.requestTimeout =
   await app.listen(3000);
 }
