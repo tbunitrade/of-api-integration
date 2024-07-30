601f68a0d5a6177769bc8bb2c8639a746554c1b8
webeng101 <jmawn101@gmail.com>
2024-07-30 09:01:26 -0600

    no message

--- backend/of-software/src/automate/automate.service.ts
+++ backend/of-software/src/automate/automate.service.ts
@@ -285,7 +285,6 @@ export class AutomateService {
               else {
                 scheduledDt = new Date();
               }
-              _config = _.cloneDeep(CONFIG);
               scheduledDt.setDate(scheduledDt.getDate() + i + 1);
               for (
                 let j = 0;
@@ -293,18 +292,12 @@ export class AutomateService {
                 j++
               ) {
                 try {
+                  _config = _.cloneDeep(CONFIG);
                   const postTime = postWithTimesAndCaptions.post_times[j];
-                  console.log(
-                    'PostTime:::: ',
-                    postWithTimesAndCaptions.post_times,
-                  );
-                  console.log('PostTime:::: ', postTime);
-                  console.log('PostTime Index :::: ', j);
                   const postCaptions = postWithTimesAndCaptions.captions;
                   if (!postCaptions) return;
                   if (postCaptions.length === 0) return;
                   if (!postTime) continue;
-                  console.log('PostTime:::: ', postTime.time);
                   const [_hour, minutes, secs] = postTime.time?.split(':');
                   const hour =
                     ((parseInt(_hour) % 13) + parseInt(_hour) / 13) | 0;
