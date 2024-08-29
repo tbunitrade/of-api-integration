2b5623da8154db74b21e37d4b97f96a23413ae28
webeng101 <jmawn101@gmail.com>
2024-08-29 08:52:44 -0600

    no message

--- backend/of-software/src/automate/automate.service.ts
+++ backend/of-software/src/automate/automate.service.ts
@@ -77,7 +77,7 @@ export class AutomateService {
       await puppeteerUtil.loadCookiesFromFile(cookieFileName);
       await puppeteerUtil.reload();
       const isLoginPage = await puppeteerUtil.checkLogin();
-      let repeatCount = 10;
+      let repeatCount = 50;
 
       while (1) {
         try {
@@ -248,7 +248,7 @@ export class AutomateService {
       await puppeteerUtil.loadCookiesFromFile(cookieFileName);
       await puppeteerUtil.reload();
       const isLoginPage = await puppeteerUtil.checkLogin();
-      let repeatCount = 10;
+      let repeatCount = 50;
 
       while (1) {
         try {
