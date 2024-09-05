849886773d4cb8bd7e35208de8f82bc1f974c929
webeng101 <jmawn101@gmail.com>
2024-09-05 12:56:19 -0600

    no message

--- backend/of-software/src/automate/automate.service.ts
+++ backend/of-software/src/automate/automate.service.ts
@@ -357,7 +357,11 @@ export class AutomateService {
                     }
                     return { ...c };
                   });
-                  await puppeteerUtil.work(config);
+                  const result = await puppeteerUtil.work(config);
+                  if (result === 'browser_closed') {
+                    console.log('Browser Closed');
+                    return scheduledCount;
+                  }
                 } catch (error) {
                   console.log('Error : ', error);
                   continue;
