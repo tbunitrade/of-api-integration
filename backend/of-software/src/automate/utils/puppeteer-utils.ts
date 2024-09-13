ead52b86bbfd8bfb5bd741c7863d31704f2be11b
webeng101 <jmawn101@gmail.com>
2024-09-13 10:46:29 -0600

    no message

--- backend/of-software/src/automate/utils/puppeteer-utils.ts
+++ backend/of-software/src/automate/utils/puppeteer-utils.ts
@@ -1334,7 +1334,7 @@ export class PuppeteerUtil {
               (target) =>
                 target.type() === 'page' && target.url().endsWith('popup'),
             );
-            const popupPage = popupTarget.asPage();
+            const popupPage = await popupTarget.page();
           } catch (error) {
             console.log('Error: ', error);
           }
