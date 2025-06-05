454b68113d37055c070fadaaedcad448779e69a7
tbunitrade <tbunitrade@gmail.com>
2025-06-05 12:54:14 +0300

    Update makefile Dockerfile docker-compose.yml files for FE&BE testing with cookies methods

---
+++ backend/of-software/src/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DISPLAY=:0
+
+# Запускаем виртуальный X сервер
+Xvfb :10 -screen 0 1920x1080x16 &
+
+# Менеджер окон
+fluxbox &
+
+# VNC сервер для удаленного доступа
+x11vnc -display :10 -forever -nopw -listen 0.0.0.0 -xkb &
+
+# Запуск Nest.js
+if [ "$ENV" = "dev" ]; then
+  npm run start:dev
+else
+  npm run start:prod
+fi
