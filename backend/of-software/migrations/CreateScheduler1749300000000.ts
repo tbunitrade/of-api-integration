6a2ac3ef415cf5f898712e6604f526339d94a67f
tbunitrade <tbunitrade@gmail.com>
2025-06-09 16:05:41 +0300

    Create init migration file, save progress ++

--- backend/of-software/migrations/CreateScheduler1749300000000.ts
+++ backend/of-software/migrations/CreateScheduler1749300000000.ts
@@ -1,4 +1,5 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
 export class CreateScheduler1749300000000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
