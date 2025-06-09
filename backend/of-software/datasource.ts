f63d209a440f76976682f6bfb2010c71b9a85b6e
tbunitrade <tbunitrade@gmail.com>
2025-06-09 13:53:35 +0300

    REFACTOR datasource.ts

--- backend/of-software/datasource.ts
+++ backend/of-software/datasource.ts
@@ -1,35 +1,20 @@
 import { DataSource } from 'typeorm';
 
 const isDocker = process.env.DB_HOST === 'local_pgdb';
-console.log('📦 Подключение к базе:', process.env.DB_NAME);
+const dbName = process.env.DB_NAME || 'of_software';
+
+console.log('📦 Подключение к базе:', dbName);
+
 export const config = new DataSource({
   type: 'postgres',
   host: isDocker ? 'local_pgdb' : 'localhost',
   port: 5432,
   username: 'postgres',
   password: isDocker ? 'supersecretpassword' : 'postgres',
-  database: 'of_software',
+  database: dbName,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['migrations/*.ts'],
   migrationsTableName: 'migrations_TypeORM',
   synchronize: false,
   logging: true,
 });
-
-
-
-
-
-// export const config = new DataSource({
-//   type: 'postgres',
-//   host: 'localhost',
-//   port: 5432,
-//   username: 'postgres',
-//   password: 'postgres',
-//   database: 'of_software',
-//   entities: ['dist/**/*.entity{.ts,.js}'],
-//   migrations: ['migrations/*.ts'],
-//   migrationsTableName: 'migrations_TypeORM',
-//   synchronize: false,
-//   logging: true,
-// });
