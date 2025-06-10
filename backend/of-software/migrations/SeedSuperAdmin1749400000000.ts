4b37511d086df8e7e8b0a1fb5eac99b5caffa276
tbunitrade <tbunitrade@gmail.com>
2025-06-10 11:50:02 +0300

    Create start Seeder for SuperAdmin creation.

---
+++ backend/of-software/migrations/SeedSuperAdmin1749400000000.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+import * as bcrypt from "bcrypt";
+
+export class SeedSuperAdmin1749400000000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const email = 'jcosta@costaindustries.com';
+
+    // Проверяем, есть ли уже такой юзер
+    const userExists = await queryRunner.query(
+      `SELECT 1 FROM "user" WHERE email = $1`,
+      [email]
+    );
+
+    if (userExists.length === 0) {
+      const passwordHash = await bcrypt.hash('abcd1234', 10);
+
+      await queryRunner.query(
+        `INSERT INTO "user" (email, password, "firstName", "lastName", "prokey") VALUES ($1, $2, $3, $4, $5)`,
+        [email, passwordHash, 'John', 'Costa', 'superadmin']
+      );
+
+      console.log('Super admin user created');
+    } else {
+      console.log('Super admin user already exists');
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Не удаляем супер-админа при откате, но можно добавить если нужно
+  }
+}
