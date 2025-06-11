0d870ab4b966ac0a5ecd2aa990c28ea26fa2680c
tbunitrade <tbunitrade@gmail.com>
2025-06-11 11:16:02 +0300

    Cookies issues v 0.0.0.9 fixing cookies loading for current session

--- backend/of-software/src/automate/utils/puppeteer-utils.ts
+++ backend/of-software/src/automate/utils/puppeteer-utils.ts
@@ -120,8 +120,8 @@ export class PuppeteerUtil {
   // 4) Установка одного cookie (если нужно вручную)
   async setCookie(name: string, value: string, domain: string) {
     if (!this._page) throw new Error('Page is not initialized');
-    const contex = this._page.browserContext();
-    contex.setCookie({ name, value, domain });
+    const context = this._page.browserContext();
+    context.setCookie({ name, value, domain });
   }
 
   // 5) Переход на любую страницу + сразу закрытие баннера cookie
