219ce998be57b7f2dd3b2c2ffce281ce33038040
tbunitrade <tbunitrade@gmail.com>
2025-06-13 09:00:55 +0300

    Avoid captcha issues setUserAgent

--- backend/of-software/src/automate/utils/puppeteer-utils.ts
+++ backend/of-software/src/automate/utils/puppeteer-utils.ts
@@ -115,6 +115,19 @@ export class PuppeteerUtil {
     });
 
     this._page = await this._browser.newPage();
+
+    const userAgents = [
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:92.0) Gecko/20100101 Firefox/92.0',
+      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    ];
+
+    const index = new Date().getSeconds() % userAgents.length;
+    const selectedUA = userAgents[index];
+    console.log('👉 Set user-agent:', selectedUA);
+
+    await this._page.setUserAgent(selectedUA);
+
     await this._page.setViewport({ width: 1920, height: 1080 });
     // Небольшая пауза (для отладки)
     console.log('>>> Жду 5 секунд перед дальнейшими действиями');
