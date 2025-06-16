8ecff1164749cb37c9df4fb428c815d701fc0d44
tbunitrade <tbunitrade@gmail.com>
2025-06-16 12:27:53 +0300

    Autoposting Done

--- backend/of-software/src/automate/utils/puppeteer-utils.ts
+++ backend/of-software/src/automate/utils/puppeteer-utils.ts
@@ -86,7 +86,6 @@ export class PuppeteerUtil {
 
     const ext = path.resolve(__dirname, '../../../extensions/hcapt/0.4.1_0');
     console.log('EXTENSION PATH for extensions/hcapt/0.4.1_0:', ext);
-    console.log('>>> Я точно собираюсь запустить Puppeteer.launch() …');
     this._browser = await this._puppeteer.launch({
       headless: this.headless,
       slowMo: 50,
