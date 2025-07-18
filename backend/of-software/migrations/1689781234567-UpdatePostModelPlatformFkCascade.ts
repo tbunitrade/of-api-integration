cb5d24cdb1e60abd68b5261002464a8b742f77ac
tbunitrade <tbunitrade@gmail.com>
2025-07-18 09:56:09 +0300

    do format for  TYpE ORMError 0.0.0.2

--- backend/of-software/migrations/1689781234567-UpdatePostModelPlatformFkCascade.ts
+++ backend/of-software/migrations/1689781234567-UpdatePostModelPlatformFkCascade.ts
@@ -1,4 +1,5 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface  } from 'typeorm';
+import { QueryRunner } from 'typeorm';
 
 export class UpdatePostModelPlatformFkCascade1689781234567  implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
