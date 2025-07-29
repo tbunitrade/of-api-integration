7b48ec10d6bb1296b3ef370ec2fc733c272f7e97
tbunitrade <tbunitrade@gmail.com>
2025-07-29 17:29:12 +0300

    Fix path issues

--- backend/of-software/backup.sh
+++ backend/of-software/backup.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-# Переходим в каталог проекта
 cd /Users/oleksandrsonich/sites/joefans/backend/of-software || exit 1
 
-# Load variables data from .env
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-
-# Path for backup folder
 BACKUP_DIR="./dailybackup"
-
-# create folder if not exist
 mkdir -p "$BACKUP_DIR"
-
-# Dump name with current date
 DUMP_FILE="$BACKUP_DIR/init-db_$(date +%F.dump)"
 
-#
-# Запускаем pg_dump из контейнера, сохраняем на локалке
-docker exec -e PGPASSWORD=$POSTGRES_PASSWORD local_pgdb pg_dump -U $POSTGRES_USER -F c -d $DB_NAME > "$DUMP_FILE"
+docker exec local_pgdb bash -c "export PGPASSWORD='$POSTGRES_PASSWORD'; pg_dump -U '$POSTGRES_USER' -F c -d '$DB_NAME'" > "$DUMP_FILE"
 
 echo "Backup created: $DUMP_FILE"
